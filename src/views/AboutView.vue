af4e54cadba597b0a7576ec5ad8a74628103d157
GrinkoVeronika <veronika.grinko@inbox.lv>
2022-03-30 15:56:32 +0300

    k

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+      possimus mollitia repellendus impedit facere nam, hic accusamus quae.
+      Tenetur eligendi pariatur nihil. Aliquid, sint non. Quasi, tenetur!
+      Adipisci, recusandae necessitatibus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+      consectetur quas quam ex maiores mollitia dolor sit omnis eveniet
+      repellendus, illo impedit laborum maxime veniam cupiditate dolorem
+      doloribus magnam! Hic.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptate
+      nihil eveniet quos qui? Ipsum provident nemo cum quidem quaerat nam
+      exercitationem laborum quis omnis! Consequuntur placeat rem incidunt eum!
+    </p>
   </div>
 </template>
